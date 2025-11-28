30f3325d3891004a200ac1045bc0b76761e7e3bc
Omkesh Gatfane <gatfaneomkesh@gmail.com>
2025-11-28 10:05:12 +0530

    removed unused imports

--- spring-jpa/src/main/java/com/omkesh/learn/spring/entity/Address.java
+++ spring-jpa/src/main/java/com/omkesh/learn/spring/entity/Address.java
@@ -5,13 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Generated;
+
 
 @Entity
 @Data
 public class Address {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String street;
     private String city;
